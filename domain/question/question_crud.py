aece1333ad2705f97bbeb172ae2cd4617a26df47
Jeon_minGyu <coby4313@gmail.com>
2023-01-26 00:26:22 +0900

    question 상세 구현

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -5,3 +5,7 @@ def get_question_list(db: Session):
     print(db)
     question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return question_list
+
+def get_question(db: Session, question_id: int):
+    question = db.query(Question).get(question_id)
+    return question
--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -6,7 +6,7 @@ from database import SessionLocal, get_db
 from sqlalchemy.orm import Session
 from models import Question
 
-from domain.question import question_crud
+from domain.question import question_schema, question_crud
 
 
 router = APIRouter(
@@ -18,3 +18,9 @@ router = APIRouter(
 def question_list(db: Session = Depends(get_db)):
     _question_list = question_crud.get_question_list(db)
     return _question_list
+
+@router.get("/detail/{question_id}")
+def question_detail(question_id: int, db: Session = Depends(get_db)):
+    
+    question = question_crud.get_question(db, question_id=question_id)
+    return question
